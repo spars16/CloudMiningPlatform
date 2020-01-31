43a6afdcb780ca7c432136cc9550afd92653a9a8
Sparsh Srivastava <spars16@vt.edu>
2020-01-31 15:57:36 -0500

    chain resolution added

--- blockchain.py
+++ blockchain.py
@@ -1,12 +1,19 @@
 import hashlib
 import json
 from time import time
+from urllib.parse import urlparse
+import request
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.transactions = []
+        self.nodes = set()
         self.new_block(proof = 100, previous_hash = 1) #genesis (first) block
+
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
     
     def new_block(self, proof, previous_hash = None):
         block = {
@@ -54,3 +61,51 @@ class Blockchain(object):
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
      # ----------------------------------------------------
+
+
+     def valid_chain(self, chain):
+         last_block = chain[0]
+         current_index = 1
+
+         while len(chain) > current_index:
+            block = chain[current_index]
+
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+
+            # verify hash of block
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            # verify proof of work
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+            last_block = block
+            current_index += 1
+        return True
+    
+    def resolve_conflicts(self):
+        # Consensus Algorithm, resolves conflicts by defaulting to the longest chain
+
+        neighbors = self.nodes
+        # only looking at chains longer than the current chain
+        max_length = len(self.chain)
+
+        # verify all chains on the network
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # replace chain if a longer valid chain is found
+        if new_chain:
+            self.chain = new_chain
+            return True
+        return False # else return false
